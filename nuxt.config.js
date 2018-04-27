c3bdd0f8b024d390be28af1ec9c671aaf92139f9
AJ Schmidt <ajschmidt@carfax.com>
2018-04-27 09:52:21 -0400

    initial push

---
+++ nuxt.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    modules: [
+		// Simple usage
+		"@nuxtjs/dotenv",
+    ],
+}
---
+++ pages/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="flex">
+      <div class="container">
+        <button @click="backendCall">
+            Click me!
+        </button>
+        <p v-if="message">
+            {{message}}
+        </p>
+      </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    data() {
+        return {
+            "message": ""
+        }
+    },
+    methods: {
+        backendCall() {
+            return axios.get(process.env.helloEndpoint)
+            .then(resp => {
+                return this.message = resp.data.message;
+            })
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    .flex {
+        height: 100vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    button {
+        background-color: red;
+        font-size: 28px;
+    }
+</style>
